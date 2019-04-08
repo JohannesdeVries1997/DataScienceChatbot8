4f3156a00033f8e35bce39ab3083956acbdd5492
sjimmie bleeker <blee1703@student.nhl.nl>
2019-04-08 13:54:23 +0200

    R script runnen

--- resources/R/video-format.R
+++ resources/R/video-format.R
@@ -1,12 +1,13 @@
 #!/usr/bin/Rscript
 
 # install.packages("RMySQL")
-library(RMySQL)
+#library(RMySQL)
 
-con <- dbConnect(MySQL(), dbname="imdb", user="imdb", password="imdb")
-values <- dbGetQuery(con, "select movie_info.info as format, count(*) as freq from movie_info, info_type where info_type.info = 'LD picture format' and info_type_id = info_type.id group by movie_info.info")
-
-invisible(jpeg('/tmp/video-format.jpg'))
-barplot(values$freq, names.arg = values$format, horiz=FALSE, cex.names=0.5)
-invisible(dev.off())
+#con <- dbConnect(MySQL(), dbname="imdb", user="imdb", password="imdb")
+#values <- dbGetQuery(con, "select movie_info.info as format, count(*) as freq from movie_info, info_type where info_type.info = 'LD picture format' and info_type_id = info_type.id group by movie_info.info")
+setwd("C:/Users/sjimm/Documents")
+crimepubs <- read.csv("crimepubs.csv")
+jpeg("rplot.jpg", width = 350, height = 350)
+plot(crimepubs)
+dev.off()
 
